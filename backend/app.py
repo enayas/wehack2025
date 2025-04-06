3c2a60ef79ba42d2d52dec70cffac95dfe208457
Ruba Osman <rubaosman10@gmail.com>
2025-04-05 20:07:40 -0500

    Initialize database

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,14 @@
 from flask import Flask
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account.
+cred = credentials.Certificate('memory-lane-db.json')
+app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
 
 app = Flask(__name__)
 @app.route('/')
